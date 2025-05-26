45cc0e38eb08c082b899f47d42c9c7fb6adf9e58
tamoyou1230 <tamoyou1230@gmail.com>
2025-05-26 12:19:46 +0900

    練習４－２

--- src/RectIf.java
+++ src/RectIf.java
@@ -5,14 +5,14 @@ public class RectIf extends MyFrame{
 		setColor(0,128,0);
 		int i;
 		int x=30;
-		int y=50;
+		int y=0;
 		
 		for(i=0; i<9 ; i++) {
 			if(i==1) {
-				fillRect(x,y,10,100);
+				fillRect(x,50,10,10+y);
 			}
 			else{
-				fillRect(x,y,10,100);
+				fillRect(x,50,10,10+y);
 			}
 			x+=20;
 			y+=20;
