aaa3b892da6934e44278fbd0fa5895c287f19508
tamoyou1230 <tamoyou1230@gmail.com>
2025-05-27 12:14:31 +0900

    練習問題５ーEX

--- src/Animation.java
+++ src/Animation.java
@@ -57,7 +57,7 @@ public class Animation extends MyFrame{
 			y-=5;
 			sleep(0.1);
 		}*/
-		int x=50;
+		/*int x=50;
 		int i;
 		
 		for(i=0;i<3;i++) {
@@ -81,6 +81,47 @@ public class Animation extends MyFrame{
 				sleep(0.1);
 			}
 			
+		}*/
+		int x=50;
+		int y=200;
+		while(true){
+			
+			while(y>=50){
+				
+				clear();
+				
+				setColor(0,128,0);
+				fillOval(x,y,100,100);
+				y-=5;
+				sleep(0.1);
+			}
+			while(x<=200){
+				
+				clear();
+				
+				setColor(0,128,0);
+				fillOval(x,y,100,100);
+				x+=5;
+				sleep(0.1);
+			}
+			while(y<=200){
+				
+				clear();
+				
+				setColor(0,128,0);
+				fillOval(x,y,100,100);
+				y+=5;
+				sleep(0.1);
+			}
+			while(x>=50){
+				
+				clear();
+				
+				setColor(0,128,0);
+				fillOval(x,y,100,100);
+				x-=5;
+				sleep(0.1);
+			}
 		}
 	}
 
