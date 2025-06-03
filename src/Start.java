250ce8f172551342ea80661181ab42ffe56fa1a9
tamoyou1230 <tamoyou1230@gmail.com>
2025-06-03 11:12:29 +0900

    DisplayFaceVectorの追加

--- src/Start.java
+++ src/Start.java
@@ -3,7 +3,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("でぃびじょん");
-		MyFrame f=new DisplayCarObject();
+		MyFrame f=new DisplayFaceObject();
 		
 	}
 
