8f37de573ddd3987a681036944c577a31f4d0964
tamoyou1230 <tamoyou1230@gmail.com>
2025-06-03 10:00:21 +0900

    Faceの移動

--- src/Start.java
+++ src/Start.java
@@ -3,7 +3,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("でぃびじょん");
-		MyFrame f=new DisplayFaceObject();
+		MyFrame f=new DisplayCarObject();
 		
 	}
 
