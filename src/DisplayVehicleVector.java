b17e8a080ea90d59b7282c24461e3f673d867649
tamoyou1230 <tamoyou1230@gmail.com>
2025-06-09 12:13:08 +0900

    練習問題８－１修正

--- src/DisplayVehicleVector.java
+++ src/DisplayVehicleVector.java
@@ -4,9 +4,9 @@ public class DisplayVehicleVector extends MyFrame{
 	public void run() {
 		Vector<Vehicle> vehicles=new Vector<Vehicle>();
 		vehicles.add(new Car(10,30,3,0));
-		vehicles.add(new Car(10,100,5,0));
+		vehicles.add(new Car(10,100,-5,0));
 		vehicles.add(new Train(10,180,7,0));
-		vehicles.add(new Train(10,260,4,0));
+		vehicles.add(new Train(10,260,-4,0));
 		vehicles.add(new Truck(10,320,2,0));
 		vehicles.add(new Truck(10,380,6,0));
 		
